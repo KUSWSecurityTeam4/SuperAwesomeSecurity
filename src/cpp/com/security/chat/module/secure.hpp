50e81d4e7c1e2756d52a3c0b273f5f9e0c18842d
Young&Old <58219267+YeongRoYun@users.noreply.github.com>
2022-11-26 23:05:03 +0900

    [Intended vulnerability patch] Patch environment variable (#17)

--- src/cpp/com/security/chat/module/secure.hpp
+++ src/cpp/com/security/chat/module/secure.hpp
@@ -3,6 +3,8 @@
 #include "exception.hpp"
 using namespace chat::module::exception;
 
+#include <cpprest/http_listener.h>
+
 #include <algorithm>
 #include <functional>
 #include <limits>
@@ -56,5 +58,24 @@ std::string generateFixedLengthCode(uint64_t length) {
   std::string str(length, 0);
   std::generate_n(str.begin(), length, randchar);
   return str;
+}
+
+decltype(auto) configSSL(std::string keyPath, std::string crtPath,
+                         std::string dhPath) {
+  auto config = web::http::experimental::listener::http_listener_config{};
+  config.set_ssl_context_callback([=](boost::asio::ssl::context &ctx) {
+    ctx.set_options(boost::asio::ssl::context::default_workarounds |
+                    boost::asio::ssl::context::no_sslv2 |
+                    boost::asio::ssl::context::no_tlsv1 |
+                    boost::asio::ssl::context::no_tlsv1_1 |
+                    boost::asio::ssl::context::single_dh_use);
+
+    ctx.use_certificate_chain_file(crtPath);
+    ctx.use_private_key_file(keyPath, boost::asio::ssl::context::pem);
+    ctx.use_tmp_dh_file(dhPath);
+  });
+  config.set_timeout(utility::seconds(10));
+
+  return config;
 }
 } // namespace chat::module::secure
