69486f7fee2ee484985e5d571c11fdf938488576
YeongRoYun <colabofyyr@gmail.com>
2022-11-27 15:15:35 +0900

    .

--- src/cpp/com/security/chat/dao/company/repository.hpp
+++ src/cpp/com/security/chat/dao/company/repository.hpp
@@ -7,6 +7,7 @@
 #include "../../module/all.hpp"
 using namespace chat::module::exception;
 
+#include <mysqlx/devapi/table_crud.h>
 #include <mysqlx/xdevapi.h>
 
 #include <fmt/core.h>
@@ -28,7 +29,7 @@ public:
     return instance;
   }
 
-  CompanyRepository(L repoLogger) : BaseRepository(repoLogger){};
+  CompanyRepository(L repoLogger) : BaseRepository(repoLogger, "company"){};
 
   R findByName(mysqlx::Session &session, std::string name) {
     return findBy(session, fmt::v9::format("name='{}'", name));
@@ -41,12 +42,11 @@ public:
   R save(mysqlx::Session &session, E entity) override {
     std::lock_guard<std::mutex> lock(sessionMutex);
     try {
-      auto company = std::dynamic_pointer_cast<Company>(entity);
-      auto query = fmt::v9::format("INSERT INTO company(name) VALUES('{}')",
-                                   company->getName());
-      auto stmt = session.sql(query);
-      auto result = stmt.execute();
-      return findByName(session, company->getName());
+      auto tableInsert = getTable(session, tableName).insert("name");
+      const auto company = std::dynamic_pointer_cast<Company>(entity);
+      const auto row = mysqlx::Row(company->getName());
+      const auto result = tableInsert.values(row).execute();
+      return findById(session, result.getAutoIncrementValue());
     } catch (const std::exception &e) {
       auto msg = fmt::v9::format("CompanyRepository: {}", e.what());
       repoLogger->error(msg);
@@ -57,13 +57,13 @@ public:
   R update(mysqlx::Session &session, E entity) override {
     std::lock_guard<std::mutex> lock(sessionMutex);
     try {
+      auto tableUpdate = getTable(session, tableName).update();
       auto company = std::dynamic_pointer_cast<Company>(entity);
-      auto query = fmt::v9::format("UPDATE company SET name='{}', "
-                                   "last_modified_at=NOW() WHERE company_id={}",
-                                   company->getName(), company->getId());
-      auto stmt = session.sql(query);
-      auto result = stmt.execute();
-
+      const auto condition = fmt::v9::format("company_id={}", company->getId());
+      const auto result = tableUpdate.set("name", company->getName())
+                              .set("last_modified_at", "expr(NOW())")
+                              .where(condition)
+                              .execute();
       return findById(session, company->getId());
     } catch (const std::exception &e) {
       auto msg = fmt::v9::format("CompanyRepository: {}", e.what());
@@ -75,11 +75,10 @@ public:
   bool remove(mysqlx::Session &session, E entity) override {
     std::lock_guard<std::mutex> lock(sessionMutex);
     try {
+      auto tableRemove = getTable(session, tableName).remove();
       auto company = std::dynamic_pointer_cast<Company>(entity);
-      auto query = fmt::v9::format("DELETE FROM company WHERE company_id={}",
-                                   company->getId());
-      auto stmt = session.sql(query);
-      auto result = stmt.execute();
+      const auto condition = fmt::v9::format("company_id={}", company->getId());
+      const auto result = tableRemove.where(condition).execute();
       return true;
     } catch (const std::exception &e) {
       auto msg = fmt::v9::format("CompanyRepository: {}", e.what());
@@ -93,20 +92,24 @@ private:
   static std::mutex createMutex;
   CompanyRepository() = delete;
 
-  R findBy(mysqlx::Session &session, std::string condition) {
+  R findBy(mysqlx::Session &session, const std::string condition) {
     try {
-      auto query = fmt::v9::format(
-          "SELECT name, company_id, {}, {} FROM company WHERE {}",
-          getUnixTimestampFormatter("created_at"),
-          getUnixTimestampFormatter("last_modified_at"), condition);
+      auto tableSelect =
+          getTable(session, tableName)
+              .select("name", "company_id",
+                      getUnixTimestampFormatter("created_at"),
+                      getUnixTimestampFormatter("last_modified_at"));
+      auto result = tableSelect.where(condition).execute();
 
-      auto stmt = session.sql(query);
-      auto result = stmt.execute();
-
-      // SELECT로 선택된 ROW가 없다면 Data는 있되, NULL ROW가 담긴다.
+      if (result.count() != 1) {
+        const auto msg = fmt::v9::format(
+            "CompanyRepository : more than one rows are selected");
+        throw EntityException(msg);
+      }
       auto row = result.fetchOne();
+
       if (row.isNull() != true) {
-        auto entity = R(new Company(
+        const auto entity = R(new Company(
             std::string(row.get(0)), uint64_t(row.get(1)),
             convertToTimeT(row.get(2)), convertToTimeT(row.get(3))));
         return entity;
